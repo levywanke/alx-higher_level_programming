060bce23074eebea46db048d3f824a57701cf65e
levywanke <levywanke@gmail.com>
2023-11-27 22:42:39 +0300

    python reloaded

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - looks for recurrence
+ * @list: start of linked list
+ *
+ * Description - validation of loops
+ * Return: return 1 or zero
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (!list)
+	{
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - outputs all the elements
+ * @h: directs to the list
+ * Return: counted nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current;
+	unsigned int n;
+
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+	return (n);
+}
+
+/**
+ * add_nodeint - addition of a new node
+ * @head: directs to the pointer start
+ * @n: merical
+ * Return: address of new inclusion
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * free_listint - release list
+ * @head: direction to list released
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
