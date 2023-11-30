5bc755bd43d50f74e4c30d54dd8efe2b014b1f03
levywanke <levywanke@gmail.com>
2023-11-30 22:50:05 +0300

    python module and import

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My func
+
+    Arguments:
+        a: fs int
+        b: sec int
+
+    Return:
+        a + b
+    """
+    return (a + b)
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """I add funcs
+
+    arguments:
+        a: fs int
+        b: sec int
+
+    return:
+        a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """I sub funcs
+
+    arguments:
+        a: fs int
+        b: sec int
+
+    return:
+        a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """I mult funcs
+
+    arguments:
+        a: fs int
+        b: sec int
+
+    return:
+        a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """I div funcs
+
+    arguments:
+        a: fs int
+        b: sec int
+
+    return:
+        a / b
+    """
+    return int(a / b)
